e2ef8592381bc0a099fc6bcc84282704e803a060
crl1005 <carlosesguerra001@gmail.com>
2026-07-06 16:15:47 +0800

    fix deplopment

--- love-folder/next.config.ts
+++ love-folder/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/celebrate",
+  assetPrefix: "/celebrate/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
