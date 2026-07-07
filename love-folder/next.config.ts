fec710f83877ddfd83b60ac86639191f61eb81c4
crl1005 <carlosesguerra001@gmail.com>
2026-07-07 16:14:16 +0800

    remove basepath

--- love-folder/next.config.ts
+++ love-folder/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/celebrate",
-  assetPrefix: "/celebrate/",
   images: {
     unoptimized: true,
   },
